05caed006fa4f9854f462457f9d9d56089339c77
tanmay kumar chaurasia <tanmaychaurasia88@gmail.com>
2025-09-30 02:51:23 +0530

    created outline of depth and delete order

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
-use actix_web::{App, HttpServer, Responder, post};
+use actix_web::{delete, get, post, App, HttpServer, Responder};
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(move || App::new().service(create_order))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .service(create_order)
+            .service(delete_order)
+            .service(get_depth)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
 
 #[post("/order")]
 async fn create_order() -> impl Responder {
     "this is create order route handler"
 }
+
+#[delete("order")]
+async fn delete_order() -> impl Responder {
+    "this is the delete order route handler"
+}
+
+#[get("/depth")]
+async fn get_depth() -> impl Responder {
+    "this is the get depth route handler"
+}
+
