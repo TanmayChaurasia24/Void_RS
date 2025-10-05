f6ae8af401d2101205a6ff524d084a610ede197f
tanmay kumar chaurasia <tanmaychaurasia88@gmail.com>
2025-10-05 22:42:13 +0530

    written order book

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use actix_web::{delete, get, post, web::Json, App, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
+use std::sync::{Arc, Mutex};
+
+use actix_web::{App, HttpServer};
+
+use crate::{orderbook::Orderbook, route::{create_order, delete_order, get_depth}};
+
+pub mod route;
+pub mod input;
+pub mod output;
+pub mod orderbook;
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
+    let orderbook = Arc::new(Mutex::new(Orderbook::new()));
+
     HttpServer::new(move || {
         App::new()
+            .app_data(orderbook.clone())
             .service(create_order)
             .service(delete_order)
             .service(get_depth)
@@ -13,54 +24,3 @@ async fn main() -> Result<(), std::io::Error> {
     .run()
     .await
 }
-
-
-//------------------------------------ Create Order ------------------------------------
-#[derive(Deserialize, Debug)]
-struct CreateOrderInput {
-    price: u32,
-    quantity: u32,
-    user_id: u32,
-    order_type: OrderType
-}
-
-#[derive(Deserialize, Debug)]
-enum OrderType {
-    Buy,
-    Sell
-}
-
-#[derive(serde::Serialize, Debug)]
-struct CreateOrderResponse {
-    order_id: String,
-    status: String
-}
-
-#[post("/order")]
-async fn create_order(body: Json<CreateOrderInput>) -> impl Responder {
-    println!("{:?}", body);
-
-    let _price = body.0.price;
-    let _quantity = body.0.quantity;
-    let _user_id = body.0.user_id;
-    let _order_type = body.0.order_type;
-
-    return HttpResponse::Ok().json(CreateOrderResponse {
-        order_id: String::from("ads"),
-        status: "Order created successfully".to_string()
-    })
-}
-
-
-//------------------------------------ Delete Order ------------------------------------
-#[delete("order")]
-async fn delete_order() -> impl Responder {
-    "this is the delete order route handler"
-}
-
-//------------------------------------ Get Depth ------------------------------------
-#[get("/depth")]
-async fn get_depth() -> impl Responder {
-    "this is the get depth route handler"
-}
-
---
+++ src/route.rs
@@ -0,0 +1,40 @@
+use std::sync::{Arc, Mutex};
+
+use actix_web::{delete, get, post, web::{self, Json, Data}, HttpResponse,Responder};
+
+use crate::{input::{CreateOrderInput, DeleteOrder}, orderbook::{self, Orderbook}, output::{CreateOrderResponse, DeleteOrderResponse, Depth}};
+
+#[post("/order")]
+pub async fn create_order(body: Json<CreateOrderInput>, orderbook: Data<Arc<Mutex<Orderbook> > >) -> impl Responder {
+    let price = body.0.price;
+    let quantity = body.0.quantity;
+    let user_id = body.0.user_id;
+    let side = body.0.side;
+    
+    // maintain orderbook logic
+    let mut orderbook = orderbook.lock().unwrap();
+    orderbook.create_order(price, quantity, user_id, side);
+
+    return HttpResponse::Ok().json(CreateOrderResponse {
+        order_id: String::from("ads")
+    });
+}
+
+#[delete("/order")]
+pub async fn delete_order(Json(body): Json<DeleteOrder>, orderbook: Data<Orderbook>) -> impl Responder {
+    let order_id = body.order_id;
+    HttpResponse::Ok().json(DeleteOrderResponse {
+        filled_qty: 0,
+        average_price: 100
+    })
+}
+
+#[get("/depth")]
+pub async fn get_depth(orderbook: Data<Orderbook>) -> impl Responder {
+    let depth = orderbook.get_depth();
+    HttpResponse::Ok().json(Depth {
+        bids: vec![],
+        asks: vec![],
+        lastUpdateId: String::from("adsa")
+    })
+}
