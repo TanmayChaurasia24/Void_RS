d3e28cb44369e73487139deb40cc08230e771c91
tanmay kumar chaurasia <tanmaychaurasia88@gmail.com>
2025-10-27 21:00:41 +0530

    written wallet schema

--- entity/src/wallets.rs
+++ entity/src/wallets.rs
@@ -1,21 +1,56 @@
+use super::prelude::*;
 use sea_orm::entity::prelude::*;
 
-#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
+#[derive(Clone, Debug, DeriveIntoActiveModel, PartialEq, DeriveEntityModel, Eq)]
 #[sea_orm(table_name = "wallets")]
 pub struct Model {
-    #[sea_orm(primary_key)]
-    pub id: i32,
-    pub user_id: i32,
-    pub currency: String,
-    pub balance: String,
-    pub created_at: DateTimeWithTimeZone,
-    pub updated_at: DateTimeWithTimeZone,
+    #[sea_orm(primary_key, auto_increment = false)]
+    pub id: Uuid,
+
+    pub user_id: Uuid,
+
+    pub asset_id: Uuid,
+
+    #[sea_orm(column_type = "Decimal(Some((32,18)))")]
+    pub total_balance: Decimal,
+
+    #[sea_orm(column_type = "Decimal(Some((32,18)))")]
+    pub available_balance: Decimal,
+
+    #[sea_orm(column_type = "Decimal(Some((32,18)))")]
+    pub locked_balance: Decimal,
+
+    #[sea_orm(default_expr = "Now()")]
+    pub updated_at: ChronoDateTimeUtc,
 }
 
-#[derive(Copy,Clone,Debug,EnumIter,DeriveRelation)]
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {
-    #[sea_orm(has_one = "super::user::Entity")]
+    #[sea_orm(
+        belongs_to = "Users",
+        from = "Column::UserId",
+        to = "super::users::Column::Id"
+    )]
     User,
+
+    #[sea_orm(
+        belongs_to = "Assets",
+        from = "Column::AssetId",
+        to = "super::assets::Column::Id"
+    )]
+    Asset,
+}
+
+impl Related<Users> for Entity {
+    fn to() -> RelationDef {
+        Relation::User.def()
+    }
+}
+
+impl Related<Assets> for Entity {
+    fn to() -> RelationDef {
+        Relation::Asset.def()
+    }
 }
 
 impl ActiveModelBehavior for ActiveModel {}
